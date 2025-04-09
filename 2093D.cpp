33882935925d5606470f4edf2c7d807e3ecf04da
Charlotte <roychrltt@gmail.com>
2025-04-09 20:48:07 +0200

    use bitwise operation to do binary search

--- 2093D.cpp
+++ 2093D.cpp
@@ -8,15 +8,46 @@ static void solve(void)
 	int n, q;
 	std::cin >> n >> q;
 	long long int cells = std::pow(2, n + 1);
-	auto deal1 = [&]() -> void
+	auto deal1 = [&](void) -> void
 	{
 		long long int x, y;
 		std::cin >> x >> y;
+		x--;
+		y--;
+
+		long long num = 0;
+		for (int i = n - 1; i >= 0; --i) {
+			int cur = 1 << i;
+			if (!(x & cur) && !(y & cur))
+				num ^= 0ll << (2 * i);
+			if ((x & cur) && (y & cur))
+				num ^= 1ll << (2 * i);
+			if ((x & cur) && !(y & cur))
+				num ^= 2ll << (2 * i);
+			if (!(x & cur) && (y & cur))
+				num ^= 3ll << (2 * i);
+		}
+		std::cout << num + 1 << std::endl;
 	};
-	auto deal2 = [&]() -> void
+	auto deal2 = [&](void) -> void
 	{
 		long long int num;
 		std::cin >> num;
+		num--;
+
+		int x = 0, y = 0;
+		for (int i = n - 1; i >= 0; --i) {
+			long long cur = 3ll << (2 * i);
+			if ((num & cur) >> (2 * i) == 0)
+				x ^= 0 << i, y ^= 0 << i;
+			if ((num & cur) >> (2 * i) == 1)
+				x ^= 1 << i, y ^= 1 << i;
+			if ((num & cur) >> (2 * i) == 2)
+				x ^= 1 << i, y ^= 0 << i;
+			if ((num & cur) >> (2 * i) == 3)
+				x ^= 0 << i, y ^= 1 << i;
+		}
+		std::cout << x + 1 << ' ' << y + 1 << std::endl;
 	};
 	while (q--)
 	{
