727517f5435ad1aa49e8afc6c75ca86ca34f7071
Charlotte <roychrltt@gmail.com>
2025-06-13 00:47:20 +0200

    could've done better but Chacha I'm already proud of you

--- history/2118D.cpp
+++ history/2118D.cpp
@@ -5,7 +5,6 @@
 
 static void solve(int n, int k, std::vector<long long int>& p, std::vector<long long int> d)
 {
-	std::cout << 7 << std::endl;
 	int dir = 1;
 	long long int pos;
 	std::cin >> pos;
@@ -16,11 +15,14 @@ static void solve(int n, int k, std::vector<long long int>& p, std::vector<long 
 	{
 		if (dir == 1)
 		{
+			if (pos > p[n - 1])
+			{
+				std::cout << "YES" << std::endl;
+				return;
+			}
 			for (int i = 0; i < n; i++)
 			{
-				std::cout << i << std::endl;
 				if (p[i] < pos) continue;
-				std::cout << i << " " << pos << std::endl;
 				time += p[i] - pos;
 				pos = p[i];
 				if (time % k != d[i] && i == n - 1)
@@ -50,6 +52,11 @@ static void solve(int n, int k, std::vector<long long int>& p, std::vector<long 
 		}
 		else
 		{
+			if (pos < p[0])
+			{
+				std::cout << "YES" << std::endl;
+				return;
+			}
 			for (int i = n - 1; i >= 0; i--)
 			{
 				if (p[i] > pos) continue;
@@ -67,7 +74,6 @@ static void solve(int n, int k, std::vector<long long int>& p, std::vector<long 
 				}
 				else if (time % k == d[i] && !vis2[i])
 				{
-					std::cout << time << " " << p[i] << " " << dir << std::endl;
 					vis2[i] = 1;
 					dir = 1;
 					pos = p[i] + 1;
@@ -83,16 +89,6 @@ int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(0);
-	int n, k;
-	std::cin >> n >> k;
-	std::vector<long long int> p(n);
-	std::vector<long long int> d(n);
-	for (int i = 0; i < n; i++)
-		std::cin >> p[i];
-	for (int i = 0; i < n; i++)
-		std::cin >> d[i];
-	int q;
-	std::cin >> q;
 
 	std::cout.tie(0);
 	int t;
