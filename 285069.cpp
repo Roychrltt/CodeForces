f92e48c3863b55f81a8a2378a590d555bc6befcd
Xiaoyun Xu <xiaxu@f1br7s13.clusters.42paris.fr>
2025-09-09 17:41:19 +0200

    .

---
+++ 285069.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define __Made return
+#define in 0
+#define France__ ;
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+	int n, d;
+	std::cin >> n >> d;
+	std::vector<int> v(n);
+	for (int i = 0; i < n; i++) std::cin >> v[i];
+
+	__Made in France__
+}
--- 307094.cpp
+++ 307094.cpp
@@ -8,21 +8,35 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(0);
 	std::cout.tie(0);
-	long long int n, k;
-	std::cin >> n >> k;
-	std::vector<long long int> v(n), pre(n + 1);
-	for (int i = 0; i < n; i++)
+	int n1, n2, n3, n4;
+	std::vector<int> a(n1), b(n2), c(n3), d(n4);
+	for (int i = 0; i < n1; i++) std::cin >> a[i];
+	for (int i = 0; i < n2; i++) std::cin >> b[i];
+	for (int i = 0; i < n3; i++) std::cin >> c[i];
+	for (int i = 0; i < n4; i++) std::cin >> d[i];
+	b.push_back(INT_MAX);
+	c.push_back(INT_MAX);
+	d.push_back(INT_MAX);
+	a.push_back(INT_MAX);
+	int idx1 = 0, idx2 = 0, idx3 = 0, idx4 = 0;
+	auto diff = [](std::vector<int>& v)
 	{
-		std::cin >> v[i];
-		pre[i + 1] = v[i] + pre[i];
-	}
-	long long int cnt = 0;
-	for (int i = 0; i < n; i++)
+		return std::max({v[0], v[1], v[2], v[3]}) - std::min({v[0], v[1], v[2], v[3]});
+	};
+	std::vector<int> ans = {0, 0, INT_MAX, INT_MAX}, tmp(4);
+	int mn = INT_MAX;
+	while (idx1 < n1)
 	{
-		auto idx = (std::lower_bound(pre.begin(), pre.end(), pre[i + 1] - k) - pre.begin());
-		long long int x = i - idx + 1;
-		cnt += x * (x + 1) / 2;
+		while (idx2 < n2 && b[idx2 + 1] < a[idx1]) idx2++;
+		while (idx3 < n3 && c[idx3 + 1] < a[idx1]) idx3++;
+		while (idx4 < n4 && d[idx4 + 1] < a[idx1]) idx4++;
+		tmp[0] = idx1 < n1 ? a[idx1] : a[idx1 - 1];
+		tmp[1] = idx2 < n2 ? b[idx2] : b[idx2 - 1];
+		tmp[2] = idx3 < n3 ? c[idx3] : c[idx3 - 1];
+		tmp[3] = idx4 < n4 ? d[idx4] : d[idx4 - 1];
+		if (diff(tmp) < diff(ans)) ans = tmp;
 	}
-	std::cout << cnt << std::endl;
+	std::copy(ans.begin(), ans.end(), std::ostream_iterator<int>(std::cout, " "));
+	std::cout << std::endl;
 	__Made in France__
 }
