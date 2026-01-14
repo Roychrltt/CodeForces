916b1655524e1d35dadb1ec6aedb6eca6031d85a
Charlotte <roychrltt@gmail.com>
2026-01-14 21:26:48 +0100

    monostack

--- 2184E.cpp
+++ 2184E.cpp
@@ -10,27 +10,37 @@ static void solve(void)
 	std::vector<int> v(n);
 	for (int i = 0; i < n; i++)
 		std::cin >> v[i];
-	std::map<int, int> mp1, mp2;
 	for (int i = 0; i < n - 1; i++)
+		v[i] = std::abs(v[i + 1] - v[i]);
+	v.back() = 0;
+	std::vector<int> res(n - 1), r(n - 1, n - 1), l(n - 1, -1);
+	std::stack<int> s;
+	std::vector<long long int> ans(n);
+	std::vector<std::vector<int>> ma(n);
+	for (int i = 0; i < n - 1; i++)
+		ma[v[i]].push_back(i);
+	for (int i = 0; i <= n - 2; i++)
 	{
-		int x = std::abs(v[i + 1] - v[i]);
-		mp1[x] = i;
+		while (!s.empty() && v[s.top()] > v[i]) s.pop();
+		if (!s.empty()) l[i] = s.top();
+		s.push(i);
 	}
-	std::vector<long long int> ans(n + 1);
-	for (int i = n - 1; i > 0; i--)
+	while (!s.empty()) s.pop();
+	for (int i = n - 2; i >= 0; i--)
 	{
-		if (mp1.find(i) == mp1.end())
-		{
-			ans[i] = ans[i + 1];
-			continue;
-		}
-
+		while (!s.empty() && v[s.top()] >= v[i]) s.pop();
+		if (!s.empty()) r[i] = s.top();
+		s.push(i);
 	}
-		
-
-
-	std::copy(ans.begin() + 1, ans.end() - 1, std::ostream_iterator<int>(std::cout, " "));
-	std::cout << std::endl;
+	long long int cnt = 0;
+	for (int i = n - 1; i >= 1; i--)
+	{
+		for (int p : ma[i]) cnt += 1ll * (r[p] - p) * (p - l[p]);
+		ans[i] = cnt;
+	}
+	for (int i = 1; i < n - 1; i++)
+		std::cout << ans[i] << " ";
+	std::cout << ans[n - 1] << std::endl;
 }
 
 int main()
